c1eb862a19a9eb03d1d2c7cc921e0bfe792ca638
Chris Waddell <yesitschris@hotmail.com>
2024-02-03 11:33:18 +1100

    🔄 REFACTOR: Non minify JS with beautify

--- config/non-min.js
+++ config/non-min.js
@@ -1,8 +1,11 @@
 export default {
 	// prevent any compression
 	compress: false,
+	compress: {defaults: false},
 	mangle: false,
 	format: {
 		comments: /^\**!|@preserve|@license|@cc_on/i,
+		beautify: true,
+		semicolons: false,
 	},
 }
